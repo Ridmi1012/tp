332d1226dfb896cf0d82cc5bb6770d988f377ae2
Ridmi1012 <ridmikaveeshasilva@gmail.com>
2025-04-24 21:37:45 +0530

    Unauthorized and added a library

---
+++ src/app/features/unauthorized/unauthorized/unauthorized.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-unauthorized',
+  imports: [],
+  templateUrl: './unauthorized.component.html',
+  styleUrl: './unauthorized.component.css'
+})
+export class UnauthorizedComponent {
+  goBack(): void {
+    window.history.back();
+  }
+
+}
